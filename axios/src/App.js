2fa9a636f4916a15c2c3e50bcb5d1c010ee3dc74
“shamnad” <hamzashamnad123@gmail.com>
2023-12-30 18:01:54 +0530

    finish

--- axios/src/App.js
+++ axios/src/App.js
@@ -1,33 +1,33 @@
-// import axios from 'axios'
-// import { useState } from 'react'
+import axios from 'axios'
+import { useState } from 'react'
 
 
-// function App() {
-//   const [state,setState] =useState([])
+function App() {
+  const [state,setState] =useState([])
 
-//   return (
-//     <div className="App">
-//       <h1>hello world</h1>
-//       <button onClick={()=>{
-//         axios.get('https://jsonplaceholder.typicode.com/posts').then((response)=>{
-//           console.log(response.data)
-//           setState(response.data)
-//         })
-//       }}>click to load</button>
-//       {state.map((obj,index)=>{
-//         return(
-//           <div>
-//             <h1>{index+1}</h1>
-//             <h2>{obj.title}</h2>
-//             <h3>{obj.body}</h3>
-//           </div>
-//         )
-//       })}
-//     </div>
-//   );
-// }
+  return (
+    <div className="App">
+      <h1>hello world</h1>
+      <button onClick={()=>{
+        axios.get('https://jsonplaceholder.typicode.com/posts').then((response)=>{
+          console.log(response.data)
+          setState(response.data)
+        })
+      }}>click to load</button>
+      {state.map((obj,index)=>{
+        return(
+          <div>
+            <h1>{index+1}</h1>
+            <h2>{obj.title}</h2>
+            <h3>{obj.body}</h3>
+          </div>
+        )
+      })}
+    </div>
+  );
+}
 
-// export default App;
+export default App;
 
 
 
